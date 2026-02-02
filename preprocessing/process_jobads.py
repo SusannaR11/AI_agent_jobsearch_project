a98b30cb599499b05de9d7ea1fc7bdee2fd35642
Susanna Rokka <susannaajrokka@gmail.com>
2026-02-02 20:50:47 +0100

    moved duckdb processing file

---
+++ preprocessing/process_jobads.py
@@ -0,0 +1,27 @@
+import duckdb
+import pandas as pd
+from datetime import date
+from collections import Counter
+
+def jobs_to_duckdb(db_path, job_count):
+    df = pd.DataFrame(job_count, columns=["occupation_group", "count"])
+    today = date.today().isoformat()
+    df["date"] = today
+    df = df[["date", "occupation_group", "count"]]
+
+    conn = duckdb.connect(db_path)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS jobads_count (
+                 date TEXT,
+                 occupation_group TEXT,
+                 count INT
+        )
+                 """)
+    
+    # overwrite in order to avoid duplication when rerunning script
+    conn.execute("DELETE FROM jobads_count WHERE date = ?", [today])
+
+    conn.register("df", df)
+    conn.execute("INSERT INTO jobads_count SELECT * FROM df")
+    conn.close()
